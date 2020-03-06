c2179058e2545bec9a9e662caebbb6ef90887675
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-03-06 09:02:04 -0300

    finalizando as animações na página home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { state, trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    // titulo com nome e cargo
+    trigger('titulo', [
+      state('inicial', style({
+        fontSize: '1em'
+      })),
+      state('maior', style({
+        fontSize: '1.15em'
+      })),
+      transition('inicial <=> maior', [
+        animate('100ms')
+      ])
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
+
+  // Estados das animações
+  estadoTitulo: string = 'inicial';
+  estadoCategorias: string = 'inicial';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  mudaEstadoTitulo() {
+    this.estadoTitulo = 'maior';
+  }
+
+  removeEstadoTitulo() {
+    this.estadoTitulo = 'inicial';
   }
 
 }
